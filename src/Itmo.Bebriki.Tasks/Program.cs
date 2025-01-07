cbc9c90992f589ecb37b59261fa63d49bd5b0794
pchz <pchz@gmail.com>
2025-01-07 19:39:18 +0300

    Feat: настроена инфраструктура для kafka

--- src/Itmo.Bebriki.Tasks/Program.cs
+++ src/Itmo.Bebriki.Tasks/Program.cs
@@ -4,10 +4,6 @@ using Itmo.Bebriki.Tasks.Application.Extensions;
 using Itmo.Bebriki.Tasks.Infrastructure.Persistence.Extensions;
 using Itmo.Bebriki.Tasks.Presentation.Grpc.Extensions;
 using Itmo.Bebriki.Tasks.Presentation.Kafka.Extensions;
-using Itmo.Dev.Platform.BackgroundTasks.Extensions;
-using Itmo.Dev.Platform.BackgroundTasks.Hangfire.Extensions;
-using Itmo.Dev.Platform.BackgroundTasks.Hangfire.Postgres.Extensions;
-using Itmo.Dev.Platform.BackgroundTasks.Postgres.Extensions;
 using Itmo.Dev.Platform.Common.Extensions;
 using Itmo.Dev.Platform.Events;
 using Itmo.Dev.Platform.Observability;
@@ -15,8 +11,6 @@ using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddUserSecrets<Program>();
 
 builder.Services.AddOptions<JsonSerializerSettings>();
 builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<JsonSerializerSettings>>().Value);
@@ -28,14 +22,6 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructurePersistence();
 builder.Services.AddPresentationGrpc();
 builder.Services.AddPresentationKafka(builder.Configuration);
-
-builder.Services.AddPlatformBackgroundTasks(configurator => configurator
-    .UsePostgresPersistence(postgres => postgres.BindConfiguration("Infrastructure:BackgroundTasks:Persistence"))
-    .ConfigureScheduling(scheduling => scheduling.BindConfiguration("Infrastructure:BackgroundTasks:Scheduling"))
-    .UseHangfireScheduling(hangfire => hangfire
-        .ConfigureOptions(o => o.BindConfiguration("Infrastructure:BackgroundTasks:Scheduling:Hangfire"))
-        .UsePostgresJobStorage())
-    .ConfigureExecution(builder.Configuration.GetSection("Infrastructure:BackgroundTasks:Execution")));
 
 builder.Services.AddPlatformEvents(b => b.AddPresentationKafkaHandlers());
 

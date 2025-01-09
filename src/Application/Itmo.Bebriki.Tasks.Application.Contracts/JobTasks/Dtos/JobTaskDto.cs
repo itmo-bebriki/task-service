d5db573caca972c104e65ac89323ed67c1ebcd9e
pchz <pchz@gmail.com>
2025-01-09 18:44:55 +0300

    Fix: фикс JobTaskDto. Убраны TODO

--- src/Application/Itmo.Bebriki.Tasks.Application.Contracts/JobTasks/Dtos/JobTaskDto.cs
+++ src/Application/Itmo.Bebriki.Tasks.Application.Contracts/JobTasks/Dtos/JobTaskDto.cs
@@ -2,50 +2,14 @@ using Itmo.Bebriki.Tasks.Application.Models.JobTasks;
 
 namespace Itmo.Bebriki.Tasks.Application.Contracts.JobTasks.Dtos;
 
-public sealed record JobTaskDto
-{
-    public long Id { get; }
-
-    public string Title { get; }
-
-    public string Description { get; }
-
-    public long AssigneeId { get; }
-
-    public JobTaskState State { get; }
-
-    public JobTaskPriority Priority { get; }
-
-    public IReadOnlySet<long> DependOnTasks { get; }
-
-    public DateTimeOffset DeadLine { get; }
-
-    public bool IsAgreed { get; }
-
-    public DateTimeOffset UpdatedAt { get; }
-
-    public JobTaskDto(
-        long id,
-        string title,
-        string description,
-        long assigneeId,
-        JobTaskState state,
-        JobTaskPriority priority,
-        IReadOnlySet<long> dependOnTasks,
-        DateTimeOffset deadLine,
-        bool isAgreed,
-        DateTimeOffset updateAt)
-    {
-        // TODO валидация
-        Id = id;
-        Title = title;
-        Description = description;
-        AssigneeId = assigneeId;
-        State = state;
-        Priority = priority;
-        DependOnTasks = dependOnTasks;
-        DeadLine = deadLine;
-        IsAgreed = isAgreed;
-        UpdatedAt = updateAt;
-    }
-}
+public sealed record JobTaskDto(
+    long Id,
+    string Title,
+    string Description,
+    long AssigneeId,
+    JobTaskState State,
+    JobTaskPriority Priority,
+    IReadOnlySet<long> DependOnTasks,
+    DateTimeOffset DeadLine,
+    bool IsAgreed,
+    DateTimeOffset UpdatedAt);

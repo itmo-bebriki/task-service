a92a199a5a36d5459a0a564e6e2ed8885258e190
pchz <pchz@gmail.com>
2025-01-27 18:05:09 +0300

    Fix: hotfix

--- src/Infrastructure/Itmo.Bebriki.Tasks.Infrastructure.Persistence/Migrations/20250104135600_EnumJobTaskStateMigration.cs
+++ src/Infrastructure/Itmo.Bebriki.Tasks.Infrastructure.Persistence/Migrations/20250104135600_EnumJobTaskStateMigration.cs
@@ -5,7 +5,7 @@ namespace Itmo.Bebriki.Tasks.Infrastructure.Persistence.Migrations;
 
 #pragma warning disable SA1649
 [Migration(20250104135600, "enum job task state")]
-public sealed class EnumJobTaskState : SqlMigration
+public sealed class EnumJobTaskStateMigration : SqlMigration
 {
     protected override string GetUpSql(IServiceProvider serviceProvider)
     {
